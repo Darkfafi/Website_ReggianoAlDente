c6adc307041e4217eab519991f706273ea1169d7
Ramses <ik_ben_ramses@hotmail.com>
2024-11-16 18:09:22 +0100

    Working on Products Listing

--- js/custom.js
+++ js/custom.js
@@ -65,3 +65,91 @@ $(".client_owl-carousel").owlCarousel({
         }
     }
 });
+
+var initSwiper = function() {
+
+    var swiper = new Swiper(".main-swiper", {
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var category_swiper = new Swiper(".category-carousel", {
+      slidesPerView: 6,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".category-carousel-next",
+        prevEl: ".category-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 6,
+        },
+      }
+    });
+
+    var brand_swiper = new Swiper(".brand-carousel", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".brand-carousel-next",
+        prevEl: ".brand-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+        1500: {
+          slidesPerView: 4,
+        },
+      }
+    });
+
+    var products_swiper = new Swiper(".products-carousel", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".products-carousel-next",
+        prevEl: ".products-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 6,
+        },
+      }
+    });
+  }
+
+  // document ready
+  $(document).ready(function() {
+    initSwiper();
+  }); // End of a document
